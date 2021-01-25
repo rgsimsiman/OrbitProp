1ad4b63df3b50adc202ee9907a1b4e31d5b285bc
Rob Simsiman <rgsimsiman@gmail.com>
2021-01-24 22:08:49 -0800

    update bat/bash scripts to not auto quit MATLAB

--- runOrbitProp.bash
+++ runOrbitProp.bash
@@ -1,3 +1,3 @@
 #! /bin/sh
 # Shell script to execute orbit propagator
-matlab -nosplash -nodesktop -r "try; cd('C:\Users\rgsim\Documents\MATLAB\OrbitProp'); main; catch; end; quit"
+matlab -nosplash -nodesktop -r "try; cd('C:\Users\rgsim\Documents\MATLAB\OrbitProp'); main; catch; end"
--- runOrbitProp.bat
+++ runOrbitProp.bat
@@ -1,3 +1,3 @@
 @echo off
 title Batch script to execute orbit propagator
-matlab -nosplash -nodesktop -r "try; cd('C:\Users\rgsim\Documents\MATLAB\OrbitProp'); main; catch; end; quit"
+matlab -nosplash -nodesktop -r "try; cd('C:\Users\rgsim\Documents\MATLAB\OrbitProp'); main; catch; end"
