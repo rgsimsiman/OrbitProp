d5f297adebade68023a9b4e39cef4f71879517ce
Rob Simsiman <rgsimsiman@gmail.com>
2021-01-24 19:54:00 -0800

    update main script to call orbitprop function

--- main.m
+++ main.m
@@ -2,13 +2,14 @@
 clear; % Clear variables
 
 % Create input UI
-prompt = {'Enter initial position vector (rx, ry, rz):', ...
-    'Enter initial velocity vector (vx, vy, vz):', ...
-    'Enter central body:', 'Enter time step (in seconds):'};
+prompt = {'Enter initial position vector (rx, ry, rz) in km:', ...
+    'Enter initial velocity vector (vx, vy, vz) in km/s:', ...
+    'Enter central body:', 'Enter time step in s:'};
 dlgtitle = 'Input';
 dims = [1 70];
 answer = inputdlg(prompt,dlgtitle,dims);
 
+% Check for valid inputs
 if length(str2num(answer{1})) ~= 3
     error(['Input a valid initial position vector (rx, ry, rz), either comma' ...
         ' or space-separated.']);
@@ -20,4 +21,11 @@ elseif isempty(str2num(answer{3}))
 elseif length(str2num(answer{4})) ~= 1
     error('Input a valid time step value.');
 end
+
+% Run propagator
+r0 = str2num(answer{1});
+v0 = str2num(answer{2});
+centralbody = answer{3};
+dt = str2num(answer{4});
+orbitprop(r0, v0, centralbody, dt);
 
